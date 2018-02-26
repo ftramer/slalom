36acbc2a817f382997bc8f321e4d21f75fda7fac
tramer <tramer@jamie.stanford.edu>
2018-02-26 10:09:24 -0800

    quant test

--- slalom/quant.py
+++ slalom/quant.py
@@ -1,11 +1,16 @@
+import tensorflow as tf
 from keras.layers import Conv2D, Dense
 import keras.backend as K
 
+QUANTIZE = True
+PRINT = False
 
 class Conv2DQ(Conv2D):
     def __init__(self, filters, kernel_size, bits_w=8, bits_x=8, **kwargs):
         super(Conv2DQ, self).__init__(filters, kernel_size, **kwargs)
-
+        self.quantize = QUANTIZE
+        self.bits_w = bits_w
+        self.bits_x = bits_x
         self.range_w = 2**bits_w
         self.range_x = 2**bits_x
         self.kernel_q = None
@@ -13,10 +18,17 @@ class Conv2DQ(Conv2D):
 
     def build(self, input_shape):
         super(Conv2DQ, self).build(input_shape)
-        self.kernel_q = K.round(self.range_w * self.kernel)
-        self.bias_q = K.round(self.range_w * self.range_x * self.bias)
+        if self.quantize:
+            self.kernel_q = K.round(self.range_w * self.kernel)
+            if self.use_bias:
+                self.bias_q = K.round(self.range_w * self.range_x * self.bias)
 
-    def call(self, inputs, blind=True, bias=True):
+        else:
+            self.kernel_q = self.kernel
+            if self.use_bias:
+                self.bias_q = self.bias
+
+    def call(self, inputs):
 
         outputs = K.conv2d(
             inputs,
@@ -26,10 +38,28 @@ class Conv2DQ(Conv2D):
             data_format=self.data_format,
             dilation_rate=self.dilation_rate)
 
-        if bias:
+        if PRINT:
+            outputs = tf.Print(outputs, [tf.reduce_max(tf.abs(outputs))])
+
+        if self.use_bias:
             outputs = K.bias_add(outputs, self.bias_q, data_format=self.data_format)
 
-        return K.round(outputs / self.range_w)
+        if self.activation is not None:
+            outputs = self.activation(outputs)
+
+        if self.quantize:
+            return K.round(outputs / self.range_w)
+        else:
+            return outputs
+
+    def get_config(self):
+        config = {
+            'bits_w': self.bits_w,
+            'bits_x': self.bits_x
+        }
+        
+        base_config = super(Conv2DQ, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
 
 
 class DenseQ(Dense):
@@ -37,6 +67,9 @@ class DenseQ(Dense):
     def __init__(self, units, bits_w=8, bits_x=8, **kwargs):
         super(DenseQ, self).__init__(units, **kwargs)
 
+        self.quantize = QUANTIZE
+        self.bits_w = bits_w
+        self.bits_x = bits_x
         self.range_w = 2**bits_w
         self.range_x = 2**bits_x
         self.kernel_q = None
@@ -44,14 +77,40 @@ class DenseQ(Dense):
 
     def build(self, input_shape):
         super(DenseQ, self).build(input_shape)
-        self.kernel_q = K.round(self.range_w * self.kernel)
-        self.bias_q = K.round(self.range_w * self.range_x * self.bias)
+        if self.quantize:
+            self.kernel_q = K.round(self.range_w * self.kernel)
+            if self.use_bias:
+                self.bias_q = K.round(self.range_w * self.range_x * self.bias)
+
+        else:
+            self.kernel_q = self.kernel
+            if self.use_bias:
+                self.bias_q = self.bias
 
     def call(self, inputs, blind=True, bias=True):
 
         outputs = K.dot(inputs, self.kernel_q)
 
-        if bias:
+        if PRINT:
+            outputs = tf.Print(outputs, [tf.reduce_max(tf.abs(outputs))])
+
+        if self.use_bias:
             outputs = K.bias_add(outputs, self.bias_q)
 
-        return K.round(outputs / self.range_w)
+        if self.activation is not None:
+            outputs = self.activation(outputs)
+
+        if self.quantize:
+            return K.round(outputs / self.range_w)
+        else:
+            return outputs
+
+    def get_config(self):
+        config = {
+            'bits_w': self.bits_w,
+            'bits_x': self.bits_x
+        }
+
+        base_config = super(DenseQ, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
--- slalom/scripts/test_quant.py
+++ slalom/scripts/test_quant.py
@@ -12,7 +12,9 @@ import sys
 from keras.applications.vgg16 import VGG16
 from keras.applications.inception_v3 import InceptionV3
 from keras.applications.imagenet_utils import preprocess_input
-from keras.layers import Conv2D, Dense
+from keras.layers import Conv2D, Dense, Lambda
+from keras.layers.normalization import BatchNormalization
+from keras.models import clone_model
 from keras import Model
 import keras.backend as K
 
@@ -29,14 +31,65 @@ def size_to_mb(s, type_bytes=4):
     return (type_bytes * s) / (1.0 * 1024**2)
 
 
-def quantize(model):
-    layers = model.layers
-    x = layers[0].input
-    for i in range(0, len(layers)):
-        x = layers[i](x)
+def quantize(model, bits_w, bits_x):
+    layer_map = {}
 
-    # Final touch
-    result_model = Model(input=layers[0].input, output=x)
+    for i, layer in enumerate(model.layers):
+        if isinstance(layer, Conv2D):
+            if (i < len(model.layers) -1) and isinstance(layer.outbound_nodes[0].outbound_layer, BatchNormalization):
+                assert not layer.use_bias
+                layer.add_weight(shape=(layer.filters,),
+                                initializer=layer.bias_initializer,
+                                name='bias',
+                                regularizer=layer.bias_regularizer,
+                                constraint=layer.bias_constraint)
+                layer.use_bias = True
+
+            conf = layer.get_config()
+            conf['bits_w'] = bits_w
+            conf['bits_x'] = bits_x
+            layer_map[layer] = Conv2DQ.from_config(conf)
+        elif isinstance(layer, Dense):
+            conf = layer.get_config()
+            conf['bits_w'] = bits_w
+            conf['bits_x'] = bits_x
+            if conf.get('activation') == 'softmax':
+                del conf['activation']
+            layer_map[layer] = DenseQ.from_config(conf)
+        elif isinstance(layer, BatchNormalization):
+            conv = layer.inbound_nodes[0].inbound_layers[0]
+            assert isinstance(conv, Conv2D)
+            assert layer.axis == 3
+
+            mean = layer.moving_mean
+            var = layer.moving_variance
+            beta = layer.beta if layer.beta is not None else 0.0
+            gamma = layer.gamma if layer.gamma is not None else 1.0
+
+            w = conv.get_weights()[0]
+            
+            new_w = K.get_session().run(w * gamma / (K.sqrt(var) + layer.epsilon))
+            new_b = K.get_session().run(-(mean*gamma) / (K.sqrt(var) + layer.epsilon) + beta)
+            conv.set_weights((new_w, new_b))
+            layer_map[layer] = Lambda(lambda x: x)
+
+    depth_keys = list(model.nodes_by_depth.keys())
+    depth_keys.sort(reverse=True)
+    for depth in depth_keys:
+        nodes = model.nodes_by_depth[depth]
+        for node in nodes:
+            if node.outbound_layer in layer_map:
+                node.outbound_layer = layer_map[node.outbound_layer]
+
+    result_model = clone_model(model)   
+    for i in range(len(result_model.layers)):
+        src = model.layers[i]
+        dest = result_model.layers[i]
+
+        if isinstance(src, Conv2D) or isinstance(src, Dense):
+            assert(isinstance(dest, Conv2DQ) or isinstance(dest, DenseQ))
+            dest.set_weights(src.get_weights())
+
     return result_model
 
 
@@ -53,34 +106,88 @@ def print_model_size(model):
     print("Total Size: {:.2f} MB".format(size_to_mb(tot_size)))
 
 
-def test_forward(sess, x, model, preprocess, quant=False):
+def slow_verify_linear_comps(linear_outputs):
+    for i in range(len(linear_outputs)):
+        name, X, W, Z = linear_outputs[i]
+        if len(X.shape) == 4:
+            #print("Conv2D with input {} and kernel {} to output {}".format(X.shape, W.shape, Z.shape))
+            #X.astype(np.int32)
+            #W = W.astype(np.int32).reshape((-1, W.shape[-1]))
+            #Z = Z.astype(np.int32).reshape((-1, Z.shape[-1]))
+            W = W.reshape((-1, W.shape[-1]))
+            Z = Z.reshape((-1, Z.shape[-1]))
+            X_patches = tf.extract_image_patches(X, 
+                                                 ksizes=[1, 3, 3, 1], 
+                                                 strides=[1, 1, 1, 1], 
+                                                 rates=[1, 1, 1, 1], 
+                                                 padding='SAME')
+            
+            Z2 = K.get_session().run(tf.matmul(tf.reshape(X_patches, (Z.shape[0], -1)), W))
+            norm1 = np.linalg.norm(Z)
+            norm2 = np.linalg.norm(Z2)
+            norm3 = np.linalg.norm(Z - Z2)
+            eq = np.array_equal(Z, Z2)
+            print("{:.3f}, {:.3f}, {:.3f}, {}".format(norm1, norm2, norm3, eq))
+	elif len(X.shape) == 2:
+            #print("MatMul with input {} and kernel {} to output {}".format(X.shape, W.shape, Z.shape))
+            #X = X.astype(np.int32)
+            #W = W.astype(np.int32)
+            #Z = Z.astype(np.int32)
+            
+            Z2 = K.get_session().run(tf.matmul(X, W))
+            norm1 = np.linalg.norm(Z)
+            norm2 = np.linalg.norm(Z2)
+            norm3 = np.linalg.norm(Z - Z2)
+            eq = np.array_equal(Z, Z2)
+            print("{:.3f}, {:.3f}, {:.3f}, {}".format(norm1, norm2, norm3, eq))
+        else:
+            raise ArgumentError("linear input has shape {}".format(X.shape))
 
-    model.summary()
+
+def test_forward(model, preprocess, quant=False, bits_w=8, bits_x=8, verify=False):
+
+    assert(not verify or quant)
+    old_ops = K.get_session().graph.get_operations()
+    linear_ops = K.constant(0)
+
     if quant:
-        model = quantize(model)
-        model.summary()
-
-    logits = model(x)
+       print("quantizing model...")
+       model = quantize(model, bits_w=bits_w, bits_x=bits_x)
+       print("quantization completed!")
+       if verify:
+           new_ops = [op for op in K.get_session().graph.get_operations() if op not in old_ops]
+           linear_ops = [(op.name, op.inputs[0], op.inputs[1], op.outputs[0]) for op in new_ops if op.type in ['Conv2D', 'MatMul']]
 
     dataset_images, labels = imagenet.load_validation(
         args.input_dir, args.batch_size, preprocess=preprocess)
 
     num_batches = args.max_num_batches
 
+    sess = K.get_session()
     coord = tf.train.Coordinator()
     threads = tf.train.start_queue_runners(sess=sess, coord=coord)
 
     res = Results()
+    res_verif = Results(acc=False)
 
     for i in range(num_batches):
         images, true_labels = sess.run([dataset_images, labels])
 
-        res.start_timer()
-        preds = sess.run(logits, feed_dict={x: images, K.learning_phase(): 0})
-        res.end_timer()
+        if quant:
+            images = np.round(2**bits_x * images)
 
+        res.start_timer()
+        preds, linear_outputs = sess.run([model.output, linear_ops], feed_dict={model.inputs[0]: images, K.learning_phase(): 0})
+        res.end_timer()
         res.record_acc(preds, true_labels)
         res.print_results()
+
+        if verify:
+            res_verif.start_timer()
+            slow_verify_linear_comps(linear_outputs) 
+            res_verif.end_timer(size=args.batch_size)
+            res_verif.print_results()
+
         sys.stdout.flush()
 
     coord.request_stop()
@@ -89,25 +196,27 @@ def test_forward(sess, x, model, preprocess, quant=False):
 
 def main(_):
 
-    with tf.Session() as sess:
-
         if args.model_name in ['vgg_16']:
             images = tf.placeholder(dtype=tf.float32, shape=(args.batch_size, 224, 224, 3))
             num_classes = 1000
             model = VGG16(include_top=True, weights='imagenet', input_tensor=images, input_shape=None, pooling=None, classes=num_classes)
             preprocess = preprocess_vgg
+            bits_w = 8
+            bits_x = 2
         elif args.model_name in ['inception_v3']:
             images = tf.placeholder(dtype=tf.float32, shape=(args.batch_size, 299, 299, 3))
             num_classes = 1000
             model = InceptionV3(include_top=True, weights='imagenet', input_tensor=images, input_shape=None, pooling=None, classes=num_classes)
             preprocess = lambda x: get_preprocessing('inception_v3')(x, 299, 299)
+            bits_w = 8
+            bits_x = 8
         else:
             raise AttributeError("unknown model {}".format(args.model_name))
 
         if args.test_name == 'model_size':
             print_model_size(model)
         elif args.test_name == 'forward':
-            test_forward(sess, images, model, preprocess)
+            test_forward(model, preprocess, quant=args.quant, bits_w=bits_w, bits_x=bits_x, verify=args.verify)
 
 
 if __name__ == '__main__':
@@ -125,5 +234,9 @@ if __name__ == '__main__':
                         help='How many images process at one time.')
     parser.add_argument('--max_num_batches', type=int, default=2,
                         help='Max number of batches to evaluate.')
+    parser.add_argument('--quant', action='store_true',
+                        help='Activate quantization.')
+    parser.add_argument('--verify', action='store_true',
+                        help='Activate verification.')
     args = parser.parse_args()
     tf.app.run()
